b3ad6ce0f9648771d81dcbd55db120b5864a12d0
Pedro <pgmonre@gmail.com>
2018-02-21 17:53:11 +0100

    Actualizacion Rama Pedro

--- js/mlt.js
+++ js/mlt.js
@@ -1,3 +1,4 @@
+/* Menu acordeon para Versión Móvil */
 var acc = document.getElementsByClassName("acordeon");
 var i;
 
@@ -12,3 +13,27 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+/* Menu acordeon para Versión Móvil */
+
+/* **************************************************** */
+
+$(document).ready(function(){
+  $("#feedimg").css("height", "60").css("float", "left").css("margin", "10px");
+  $("#feedtext").css("");
+});
+
+  setInterval(function(){
+    $("#feedback").fadeToggle("slow");
+    setTimeout(function(){
+      $.getJSON('./recursos/rrss.json', function(response){
+        var longJSON = Object.keys(response.feedbacks).length;
+        var random = 0;
+        random = Math.floor(Math.random()*longJSON);
+
+        $("#feedtext").text(response.feedbacks[random].feedback);
+        $("#feedimg").attr("src", response.feedbacks[random].imagen);
+        $("#feeduser").text(response.feedbacks[random].usuario);
+        $("#feedback").fadeIn("slow");
+      });
+    }, 1000);
+  }, 5000);
