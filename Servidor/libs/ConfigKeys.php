cdca7d554d5822620fbd3a03c78eb8a4ab879b3e
Pedro <pgmonre@gmail.com>
2018-02-22 15:13:59 +0100

    Actualizacion Rama Pedro

---
+++ Servidor/libs/ConfigKeys.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Claves de los valores de configuración.
+ */
+class ConfigKeys {
+
+    private function __construct() {
+        //Utility class
+    }
+
+    //DB
+    const DB_SERVER = '127.0.0.1';
+    const DB_DRIVER = 'dbdriver';
+    const DB_NAME = 'pro_cibi';
+    const DB_USER = 'root';
+    const DB_PASS = '';
+
+    //JSON cfg key
+    const JSON_TOKEN_KEY = 'jsontokenkey';
+
+}
+
+?>
